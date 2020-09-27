0d651d893aca9fa77244d5b36c952c3d40c1bc5c
salah elhossiny <salah_elhossiny856@yahoo.com>
2020-09-27 05:26:51 +0200

    hamming weight

---
+++ hamming_weight.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def hammingWeight(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        binary = '{0:08b}'.format(n)
+        
+        c = 0
+        
+        for i in binary: 
+            if i == '1':
+                c+=1
+                
+                
+        
+        
+        return c
+        
+        
