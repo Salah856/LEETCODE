1efbefffb8d5f6faae18e8412ad816138e91d8f8
salah elhossiny <salah_elhossiny856@yahoo.com>
2020-09-27 04:58:25 +0200

    longest common prefix

---
+++ longestCommonPrefix.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        prefix=[]
+        num = len(strs)
+        
+        for x in zip(*strs):
+            if len(set(x)) == 1:
+                prefix.append(x[0])
+            else:
+                break
+                
+        return "".join(prefix)
+        
