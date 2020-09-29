81e22079463224057f8d5738869cb189336fd936
salah elhossiny <salah_elhossiny856@yahoo.com>
2020-09-29 07:19:57 +0200

    edit

--- minstack.java
+++ minstack.java
@@ -1,43 +1,27 @@
 class MinStack {
-
-
     Stack <Integer> stack = new Stack();
     Stack <Integer> minVals = new Stack();
-    
-    
     public void push(int x) {
         
         if(minVals.isEmpty() || x <= minVals.peek()){
             
             minVals.push(x);
-        }
-
-            
+        }       
             stack.push(x);
-        
-    }
-    
+    }    
     public void pop() {
-        
         if(stack.peek().equals( minVals.peek())){
             minVals.pop();
         }
         
         stack.pop();
-        
-        
     }
     
     public int top() {
-        
-        return stack.peek();
-        
+        return stack.peek();   
     }
-    
     public int getMin() {
-        
         return minVals.peek();
-        
     }
 }
 
