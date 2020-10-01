9a868942b9985de69db0feaa50102d533601a94d
salah elhossiny <salah_elhossiny856@yahoo.com>
2020-10-01 08:14:16 +0200

    robot origin

---
+++ robotToOrigin.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean judgeCircle(String moves) {
+        
+        int x = 0 ; 
+        int y = 0; 
+        for (char move : moves.toCharArray()){
+            
+            switch(move){
+                    
+                case 'U':
+                    y += 1; 
+                    break; 
+                
+                case 'D':
+                    y-=1; 
+                    break; 
+                case 'L': 
+                    x-=1; 
+                    break; 
+                    
+                case 'R':
+                    x+=1; 
+                    break; 
+            }
+            
+        }
+        
+        return x == 0 && y == 0; 
+    }
+}
