8bb463868461c439f38049da2a2755604e59197c
salah elhossiny <salah_elhossiny856@yahoo.com>
2020-10-01 07:43:34 +0200

    odd even linkedlist

---
+++ oddEvenLinkedList.java
@@ -0,0 +1,24 @@
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        
+        if (head == null) return null; 
+        
+        ListNode odd = head; 
+        ListNode even = head.next; 
+        ListNode evenHead = even; 
+        
+        while(even != null && even.next != null){ 
+            
+            odd.next = even.next; 
+            odd = odd.next; 
+            
+            even.next = odd.next; 
+            even = even.next;
+        }
+        
+        odd.next = evenHead; 
+        
+        return head; 
+        
+    }
+}
