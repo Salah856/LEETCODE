1751877805084e81c625bc4fad38f319e05e1533
salah elhossiny <salah_elhossiny856@yahoo.com>
2020-10-09 06:27:35 +0200

    valid palindrome ii

---
+++ validPalindromeII.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def validPalindrome(self, s):
+        def is_pali_range(i, j):
+            return all(s[k] == s[j-k+i] for k in range(i, j))
+
+        for i in xrange(len(s) / 2):
+            if s[i] != s[~i]:
+                j = len(s) - 1 - i
+                return is_pali_range(i+1, j) or is_pali_range(i, j-1)
+        return True
