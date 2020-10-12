340da5d78710eb74d16492f2905567fe527fd413
salah elhossiny <salah_elhossiny856@yahoo.com>
2020-10-12 03:04:16 +0200

    ---

--- secondHighestSalary.sql
+++ secondHighestSalary.sql
@@ -1,7 +1,18 @@
-SELECT
-    IFNULL(
-      (SELECT DISTINCT Salary
-       FROM Employee
-       ORDER BY Salary DESC
-        LIMIT 1 OFFSET 1),
-    NULL) AS SecondHighestSalary
+# first solution: 
+
+# SELECT
+#     IFNULL(
+#       (SELECT DISTINCT Salary
+#        FROM Employee
+#        ORDER BY Salary DESC
+#         LIMIT 1 OFFSET 1),
+#     NULL) AS SecondHighestSalary
+
+###################################################
+
+
+# another solution: 
+
+SELECT MAX(Salary) AS SecondHighestSalary
+FROM Employee
+WHERE Salary < (SELECT MAX(Salary) FROM Employee)
