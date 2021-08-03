06ff503a99f9a69ae56f8ef94fe91993fd6079de
Salah Elhossiny <salah_elhossiny856@yahoo.com>
2021-08-03 12:32:16 +0200

    Create find_closest.py

---
+++ find_closest.py
@@ -0,0 +1,16 @@
+def findClosestValueInBst(tree, target):
+    return findClosestValueInBstHelper(tree, target, float("inf"))
+
+
+def findClosestValueInBstHelper(tree, target, closest):
+    currentNode = tree
+    while currentNode is not None:
+        if abs(target - closest) > abs(target - currentNode.value):
+            closest = currentNode.value
+        if target < currentNode.value:
+            currentNode = currentNode.left
+        elif target > currentNode.value:
+            currentNode = currentNode.right
+        else:
+            break
+    return closest
