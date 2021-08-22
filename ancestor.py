ca8867dc6a00d6124dfea18cad93554f57bb2cb4
Salah Elhossiny <salah_elhossiny856@yahoo.com>
2021-08-22 12:24:52 +0200

    Create ancestor.py

---
+++ ancestor.py
@@ -0,0 +1,32 @@
+​class AncestralTree:
+    def __init__(self, name):
+        self.name = name
+        self.ancestor = None
+
+
+# O(d) time | O(1) space - where d is the depth (height) of the ancestral tree
+def getYoungestCommonAncestor(topAncestor, descendantOne, descendantTwo):
+    depthOne = getDescendantDepth(descendantOne, topAncestor)
+    depthTwo = getDescendantDepth(descendantTwo, topAncestor)
+    if depthOne > depthTwo:
+        return backtrackAncestralTree(descendantOne, descendantTwo, depthOne - depthTwo)
+    else:
+        return backtrackAncestralTree(descendantTwo, descendantOne, depthTwo - depthOne)
+
+
+def getDescendantDepth(descendant, topAncestor):
+    depth = 0
+    while descendant != topAncestor:
+        depth += 1
+        descendant = descendant.ancestor
+    return depth
+
+
+def backtrackAncestralTree(lowerDescendant, higherDescendant, diff):
+    while diff > 0:
+        lowerDescendant = lowerDescendant.ancestor
+        diff -= 1
+    while lowerDescendant != higherDescendant:
+        lowerDescendant = lowerDescendant.ancestor
+        higherDescendant = higherDescendant.ancestor
+    return lowerDescendant
