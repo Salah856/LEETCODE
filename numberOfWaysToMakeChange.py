d615f6bfa286f0db950ffd91455e7d9bf4df416f
Salah Elhossiny <salah_elhossiny856@yahoo.com>
2021-08-28 18:08:29 +0200

    Create numberOfWaysToMakeChange.py

---
+++ numberOfWaysToMakeChange.py
@@ -0,0 +1,11 @@
+def numberOfWaysToMakeChange(n, denoms):
+    
+    ways = [0 for amount in range(n + 1)]
+    ways[0] = 1
+    
+    for denom in denoms:
+        for amount in range(1, n + 1):
+            if denom <= amount:
+                ways[amount] += ways[amount - denom]
+    
+    return ways[n]
