5ba4e652593c6179d765c2608da52679cc08571e
Salah Elhossiny <salah_elhossiny856@yahoo.com>
2021-08-28 18:26:03 +0200

    Create moveItemsToEnd.py

---
+++ moveItemsToEnd.py
@@ -0,0 +1,17 @@
+def moveElementToEnd(array, toMove):
+    
+    i = 0
+    j = len(array) - 1
+    
+    while i < j:
+        
+        while i < j and array[j] == toMove:
+            j -= 1
+        
+        if array[i] == toMove:
+            array[i], array[j] = array[j], array[i]
+        
+        i += 1
+    
+    return array
+  
