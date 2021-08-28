9a5ac2faf3564706e1a8a988500689bcfd182471
Salah Elhossiny <salah_elhossiny856@yahoo.com>
2021-08-28 09:29:45 +0200

    Create invertBT.py

---
+++ invertBT.py
@@ -0,0 +1,13 @@
+def invertBinaryTree(tree):
+    queue = [tree]
+    while len(queue):
+        current = queue.pop(0)
+        if current is None:
+            continue
+        swapLeftAndRight(current)
+        queue.append(current.left)
+        queue.append(current.right)
+
+
+def swapLeftAndRight(tree):
+    tree.left, tree.right = tree.right, tree.left
