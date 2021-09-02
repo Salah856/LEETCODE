996ca847bc550cb8d937f286d148b4e02e4ebf25
Salah Elhossiny <salah_elhossiny856@yahoo.com>
2021-09-02 08:33:22 +0200

    Update Thousand Separator.py

--- Thousand Separator.py
+++ Thousand Separator.py
@@ -1,26 +1,8 @@
-class Solution(object):
-    def thousandSeparator(self, n):
-        """
-        :type n: int
-        :rtype: str
-        """
-        if len(str(n))<=3:
-            return str(n)
-        
-        s = str(n)
-        s = s[::-1]
-        l = []
-        
-        for i in s:
-            l.append(i)
-            
-        i = 3
-        while i<len(l):
-            l.insert(i,'.')
-            i+=4
-            
-            
-        l = l[::-1]
-            
-        return ''.join(l)
-        
+class Solution:
+    def thousandSeparator(self, n: int) -> str:
+        n_str = [each for each in str(n)]    
+        for i in range(len(n_str)-3,0, -3):
+          n_str.insert(i, ".")
+        return "".join(n_str)
+    
+    
