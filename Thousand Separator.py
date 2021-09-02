d2d757434529823658cd00db3084599f65b8613f
Salah Elhossiny <salah_elhossiny856@yahoo.com>
2021-09-02 08:33:46 +0200

    Update Thousand Separator.py

--- Thousand Separator.py
+++ Thousand Separator.py
@@ -1,8 +1,11 @@
 class Solution:
     def thousandSeparator(self, n: int) -> str:
+        
         n_str = [each for each in str(n)]    
-        for i in range(len(n_str)-3,0, -3):
+        
+        for i in range(len(n_str) - 3, 0, -3):
           n_str.insert(i, ".")
+        
         return "".join(n_str)
     
     
