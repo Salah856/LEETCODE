820453e6af31815229622a26e0127aedb234a23b
Salah Elhossiny <salah_elhossiny856@yahoo.com>
2021-09-03 11:22:36 +0200

    Create balancedBST.py

---
+++ balancedBST.py
@@ -0,0 +1,20 @@
+class Solution:
+    
+    def height(self, root):
+        if root is None:
+            return 1
+        
+        return max(self.height(root.left), self.height(root.right)) + 1
+    
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        
+        if root is None:
+            return True
+        
+        lh = self.height(root.left)
+        rh = self.height(root.right)
+        
+        if abs(lh - rh) <= 1 and self.isBalanced(root.left) and self.isBalanced(root.right):
+            return True
+        
+        return False 
