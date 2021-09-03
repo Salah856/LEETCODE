c69a0b7200acbd30606e5f0edfc7e173cf2598db
Salah Elhossiny <salah_elhossiny856@yahoo.com>
2021-09-03 14:18:27 +0200

    Create nums_permutation.py

---
+++ nums_permutation.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def buildArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        
+        return [nums[num] for num in nums]
