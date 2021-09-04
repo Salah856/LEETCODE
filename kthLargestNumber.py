659dc42a67269e7272693e384b353f14e1c0ce76
Salah Elhossiny <salah_elhossiny856@yahoo.com>
2021-09-04 08:15:03 +0200

    Create kthLargestNumber.py

---
+++ kthLargestNumber.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def kthLargestNumber(self, nums, k):
+        """
+        :type nums: List[str]
+        :type k: int
+        :rtype: str
+        """
+        return sorted(nums, key = lambda x: (len(x), x))[-k]
+
+        
