48d248c953b92b9080bac9963eaeb088356e4680
Salah Elhossiny <salah_elhossiny856@yahoo.com>
2021-09-06 07:58:19 +0200

    Create GenTriple.py

---
+++ GenTriple.py
@@ -0,0 +1,14 @@
+def GenTriple(n):
+    n_squared = n ** 2
+    squares = set([ x ** 2 for x in range(1, n + 1)])
+    diffs = [n_squared-sqr for sqr in squares]
+    present = [diff in squares for diff in diffs]
+    
+    return sum(present) 
+
+class Solution:
+    def countTriples(self, n):
+        
+        trips = [GenTriple(x) for x in range(1,n+1)]
+        
+        return sum(trips)
