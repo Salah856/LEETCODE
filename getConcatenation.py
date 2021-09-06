b2cd7685871c47bc15a1da097f70cb0f44c3e269
Salah Elhossiny <salah_elhossiny856@yahoo.com>
2021-09-06 07:55:10 +0200

    Create getConcatenation.py

---
+++ getConcatenation.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def getConcatenation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        
+        return nums + nums
+        
