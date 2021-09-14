82fde3b25c28a7b6a0814074ded4160f7c9cfaf7
Salah Elhossiny <salah_elhossiny856@yahoo.com>
2021-09-14 09:08:01 +0200

    Create reverseOnlyLetters.py

---
+++ reverseOnlyLetters.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def reverseOnlyLetters(self, S):
+        
+        letters = [c for c in S if c.isalpha()]
+        ans = []
+        
+        for c in S:
+            
+            if c.isalpha():
+                ans.append(letters.pop())
+            
+            else:
+                ans.append(c)
+        
+        return "".join(ans)
