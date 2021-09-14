7ec9046a5fa2817e166578d674cfc55fe9ca20e2
Salah Elhossiny <salah_elhossiny856@yahoo.com>
2021-09-14 08:49:00 +0200

    Update 3sum.java

--- 3sum.java
+++ 3sum.java
@@ -2,44 +2,44 @@
 class Solution{
 
         public List<List<Integer>> threeSum(int[] nums) {
-        List list = new ArrayList();
-        if(nums.length < 3){
-            return list;
-        }
-        Arrays.sort(nums);
-        for(int i = 0;i < nums.length - 2;i++){
-            if(i > 0 && nums[i] == nums[i-1]){
-                continue;
-            }
-            int sum = nums[i];
-            int front = i+1;
-            int end = nums.length -1;
-            while(front < end){
-                if(end < nums.length -1 && nums[end] == nums[end+1]){
-                    end--;
-                    continue;
+                List list = new ArrayList();
+                if(nums.length < 3){
+                    return list;
                 }
-                if(front>i+1 && nums[front] == nums[front-1]){
-                    front++;
-                    continue;
+                Arrays.sort(nums);
+                for(int i = 0;i < nums.length - 2;i++){
+                    if(i > 0 && nums[i] == nums[i-1]){
+                        continue;
+                    }
+                    int sum = nums[i];
+                    int front = i+1;
+                    int end = nums.length -1;
+                    while(front < end){
+                        if(end < nums.length -1 && nums[end] == nums[end+1]){
+                            end--;
+                            continue;
+                        }
+                        if(front>i+1 && nums[front] == nums[front-1]){
+                            front++;
+                            continue;
+                        }
+                        if(nums[front] + nums[end] == -sum){
+                            ArrayList triplet = new ArrayList();
+                            triplet.add(sum);
+                            triplet.add(nums[front]);
+                            triplet.add(nums[end]);
+                            list.add(triplet);
+                            end--;
+                            front++;
+                        }else if(nums[front] + nums[end] > -sum){
+                            end--;
+                        }else{
+                            front++;
+                        }
+                    }
                 }
-                if(nums[front] + nums[end] == -sum){
-                    ArrayList triplet = new ArrayList();
-                    triplet.add(sum);
-                    triplet.add(nums[front]);
-                    triplet.add(nums[end]);
-                    list.add(triplet);
-                    end--;
-                    front++;
-                }else if(nums[front] + nums[end] > -sum){
-                    end--;
-                }else{
-                    front++;
-                }
-            }
-        }
-        return list;
-    }
+                return list;
+     }
 } 
 
 
