afe39d5cff46d60481085e0458c57b7d71c80f02
Salah Elhossiny <salah_elhossiny856@yahoo.com>
2021-09-27 09:02:34 +0200

    Create uniqEmail.py

---
+++ uniqEmail.py
@@ -0,0 +1,23 @@
+class Solution:
+    def numUniqueEmails(self, emails: List[str]) -> int:
+        
+        emails_new = [i.split('@') for i in emails]
+        p = []
+        
+        for i in emails_new:
+            
+            l=i[0]
+            m=l.split('+')
+            k=m[0]
+            
+            n=k.replace('.','')
+            f=n+'@'+i[1]
+            
+            p.append(f)
+        
+        return(len(set(p)))
+    
+    
+    
+    
+    
