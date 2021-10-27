fc121264290e08ad8faa0816a5e77c1ca1bc1b5e
Salah Elhossiny <salah_elhossiny856@yahoo.com>
2021-10-27 10:26:04 +0200

    peak

---
+++ peak.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public int findPeakElement(int[] nums) {
+        return search(nums, 0, nums.length - 1);
+    }
+    public int search(int[] nums, int l, int r) {
+        if (l == r)
+            return l;
+        int mid = (l + r) / 2;
+        if (nums[mid] > nums[mid + 1])
+            return search(nums, l, mid);
+        return search(nums, mid + 1, r);
+    }
+}
