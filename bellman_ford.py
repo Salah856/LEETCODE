3cb3f3240d7a95a58e6da84be9a5f9ec517b1eeb
Salah Elhossiny <salah_elhossiny856@yahoo.com>
2021-11-01 09:37:35 +0200

    Update bellman_ford.py

--- bellman_ford.py
+++ bellman_ford.py
@@ -1,5 +1,3 @@
-# Online Python compiler (interpreter) to run Python online.
-# Write Python 3 code in this online editor and run it.
 # Python3 program for Bellman-Ford's single source
 # shortest path algorithm.
 
